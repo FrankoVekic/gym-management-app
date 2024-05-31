a5d6811801ffcb7c96a3fbfb4a69963c984e4e48
Franko Vekić <franko.vekic@gmail.com>
2024-05-31 21:12:02 +0200

    Trainer controller with get all trainers and add trainer

---
+++ backend/src/main/java/com/franko/gym_management/gym_management_app/api/TrainerController.java
@@ -0,0 +1,31 @@
+package com.franko.gym_management.gym_management_app.api;
+
+import com.franko.gym_management.gym_management_app.dto.TrainerDto;
+import com.franko.gym_management.gym_management_app.service.TrainerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/public/trainers/")
+public class TrainerController {
+
+    @Autowired
+    private TrainerService trainerService;
+
+    @GetMapping("getTrainers")
+    public ResponseEntity<List<TrainerDto>> getTrainers(){
+        List<TrainerDto> trainers = trainerService.getTrainers();
+        return ResponseEntity.ok(trainers);
+    }
+
+    @PostMapping("addTrainer")
+    public ResponseEntity<TrainerDto> createTrainer(@RequestBody TrainerDto trainerDto){
+        TrainerDto savedTrainer = trainerService.createTrainer(trainerDto);
+        return new ResponseEntity<>(savedTrainer, HttpStatus.CREATED);
+    }
+
+}
