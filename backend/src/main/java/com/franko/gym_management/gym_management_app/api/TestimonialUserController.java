bec8d3b96693eb86129052e51330777f52dd02a6
Franko Vekić <franko.vekic@gmail.com>
2024-06-06 17:14:20 +0200

    TestimonialUser Controller with get all and create

---
+++ backend/src/main/java/com/franko/gym_management/gym_management_app/api/TestimonialUserController.java
@@ -0,0 +1,34 @@
+package com.franko.gym_management.gym_management_app.api;
+
+import com.franko.gym_management.gym_management_app.dto.TestimonialDto;
+import com.franko.gym_management.gym_management_app.dto.TestimonialUserDto;
+import com.franko.gym_management.gym_management_app.dto.TrainingSessionDto;
+import com.franko.gym_management.gym_management_app.service.TestimonialUserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/public/testimonialsUsers/")
+public class TestimonialUserController {
+
+    @Autowired
+    private TestimonialUserService testimonialUserService;
+
+    @GetMapping("getTestimonialUsers")
+    public ResponseEntity<List<TestimonialUserDto>> getTestimonialUsers(){
+        List<TestimonialUserDto> trainingSessions = testimonialUserService.getTestimonialUsers();
+        return ResponseEntity.ok(trainingSessions);
+    }
+
+    @PostMapping("addTestimonialUser")
+    public ResponseEntity<TestimonialUserDto> createTestimonialUser(@RequestBody TestimonialUserDto testimonialUserDto){
+        TestimonialUserDto savedTestimonialUser = testimonialUserService.createTestimonialUser(testimonialUserDto);
+        return new ResponseEntity<>(savedTestimonialUser, HttpStatus.CREATED);
+    }
+}
