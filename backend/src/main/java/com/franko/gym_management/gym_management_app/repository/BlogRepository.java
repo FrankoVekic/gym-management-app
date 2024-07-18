83a6a943ea2128cf5766889281dc72b679ccf029
Franko Vekić <franko.vekic@gmail.com>
2024-07-18 10:28:12 +0200

    Added queries for Blog filter

--- backend/src/main/java/com/franko/gym_management/gym_management_app/repository/BlogRepository.java
+++ backend/src/main/java/com/franko/gym_management/gym_management_app/repository/BlogRepository.java
@@ -3,6 +3,7 @@ package com.franko.gym_management.gym_management_app.repository;
 import com.franko.gym_management.gym_management_app.model.Blog;
 import com.franko.gym_management.gym_management_app.model.Testimonial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,19 @@ import java.util.List;
 public interface BlogRepository extends JpaRepository<Blog,Long> {
 
     List<Blog> findAllByOrderByIdDesc();
+
+    @Query(value = "SELECT * FROM blog WHERE created_at >= CURRENT_DATE - interval '7 days' ORDER BY created_at DESC", nativeQuery = true)
+    List<Blog> findFromThisWeek();
+
+    @Query(value = "SELECT * FROM blog WHERE created_at >= CURRENT_DATE - interval '1 month' ORDER BY created_at DESC", nativeQuery = true)
+    List<Blog> findFromThisMonth();
+
+    @Query(value = "SELECT b.*, COUNT(c.id) AS comment_count FROM blog b LEFT JOIN comment c ON b.id = c.blog_id GROUP BY b.id ORDER BY comment_count DESC",  nativeQuery = true)
+    List<Blog> getBlogsWithMostComments();
+
+    @Query(value = "SELECT b.*, COUNT(c.id) AS comment_count FROM blog b LEFT JOIN comment c ON b.id = c.blog_id GROUP BY b.id ORDER BY comment_count ASC",  nativeQuery = true)
+    List<Blog> getBlogsWithLeastComments();
+
+
+
 }
